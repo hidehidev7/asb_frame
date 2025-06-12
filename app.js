98f652ae4433d9b337a6d74e76dbd0f2a2f9bac8
hidehidev7 <hidehidev7@gmail.com>
2025-06-12 15:14:52 +0900

    routeファイルを削除して中身をapp.jsにまとめる

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = express.Router().get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 
 var app = express();
 
